243d55005214b33d650d778cfe3bb3f94ea95d02
Jerold Kim <iamjooyoung@gmail.com>
2018-11-11 22:20:49 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+  array = []
+  while i < collection.length 
+    if collection[i].even?
+    array << collection[i]
+    i += 1
+  end 
+  array
 end
